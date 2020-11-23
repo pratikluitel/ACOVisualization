6d84c780a7890becab535b3f7ac1a5ed70eb0cbc
Pratik Luitel <073bex426.pratik@pcampus.edu.np>
2020-11-23 08:40:27 +0545

    minor changes

--- large_example.py
+++ large_example.py
@@ -6,9 +6,12 @@ import numpy as np
 
 from ant_colony import AntColony
 
-large_distances = np.random.randint(1,10,size=(100,100))
-large_distances = (large_distances+large_distances.T).astype('float64')
-np.fill_diagonal(large_distances,float('inf'))
+# making a random large city with 100 streets using numpy
 
-large_ant_colony = AntColony(large_distances, 10, 10, 100, 0.15, alpha=1, beta=1)
+large_distances = np.random.randint(1, 10, size=(100, 100))
+large_distances = (large_distances+large_distances.T).astype('float64')
+np.fill_diagonal(large_distances, float('inf'))
+
+large_ant_colony = AntColony(
+    large_distances, 10, 10, 100, 0.15, alpha=1, beta=1)
 print(large_ant_colony.run())
